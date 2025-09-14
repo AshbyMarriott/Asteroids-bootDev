7090662f191d737a55500500f4f37b2bc9a97d14
Ashby Marriott <ashby@stevemarriott.com>
2025-09-14 15:13:18 -0400

    Added asteroid.py and asteroidfield.py. Game now generates asteroids that move across the screen.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting Asteroids!")
@@ -13,15 +15,32 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    # create sprite groups
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    # assign groups to class variables
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = updateable
+
+    # instantiate
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidField = AsteroidField()
+
+    # game loop
     while True:
+        # quit if the game window is closed
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         dt = clock.tick(60) / 1000
-        player.update(dt)
+        updateable.update(dt)
         screen.fill((0, 0, 0))
-        player.draw(screen)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
         
 
